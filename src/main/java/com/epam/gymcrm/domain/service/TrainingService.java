2d18ef65db4149d29ba00a9ad4c3bb1bcc4759a0
tutkuince <tutku.ince@outlook.com>
2025-08-05 18:22:57 +0300

    fix(training): fix missing insert into trainee-trainer many-to-many relation

--- src/main/java/com/epam/gymcrm/domain/service/TrainingService.java
+++ src/main/java/com/epam/gymcrm/domain/service/TrainingService.java
@@ -51,21 +51,25 @@ public class TrainingService {
         logger.info("Received request to add new training: traineeUsername='{}', trainerUsername='{}', trainingName='{}'",
                 request.traineeUsername(), request.trainerUsername(), request.trainingName());
 
+        // Trainee fetch
         TraineeEntity traineeEntity = traineeRepository.findByUserUsername(request.traineeUsername())
                 .orElseThrow(() -> {
                     logger.warn("Failed to add training: Trainee not found. traineeUsername='{}'", request.traineeUsername());
                     return new NotFoundException("Trainee not found: " + request.traineeUsername());
                 });
 
+        // Trainer fetch
         TrainerEntity trainerEntity = trainerRepository.findByUserUsername(request.trainerUsername())
                 .orElseThrow(() -> {
                     logger.warn("Failed to add training: Trainer not found. trainerUsername='{}'", request.trainerUsername());
                     return new NotFoundException("Trainer not found: " + request.trainerUsername());
                 });
 
+        // Domain conversion
         Trainee trainee = TraineeDomainMapper.toTraineeShallow(traineeEntity);
         Trainer trainer = TrainerDomainMapper.toTrainerShallow(trainerEntity);
 
+        // Date parse
         Training training;
         try {
             training = new Training(
@@ -81,15 +85,23 @@ public class TrainingService {
             throw new BadRequestException("Invalid trainingDate format. Must be yyyy-MM-dd HH:mm:ss");
         }
 
+        // Map to entity
         TrainingEntity trainingEntity = TrainingDomainMapper.toTrainingEntity(training);
 
+        traineeEntity.getTrainers().add(trainerEntity);
+        traineeRepository.save(traineeEntity);
+
+        // Trainer availability check
         checkTrainerAvailability(trainer.getId(), training.getTrainingDate());
-        trainingRepository.save(trainingEntity);
+
+        // Save training
+        TrainingEntity saved = trainingRepository.save(trainingEntity);
         metrics.incrementCreated();
 
         logger.info("Training added successfully: trainingId={}, trainingName='{}', traineeUsername='{}', trainerUsername='{}'",
                 trainingEntity.getId(), trainingEntity.getTrainingName(), request.traineeUsername(), request.trainerUsername());
     }
+
 
     private void checkTrainerAvailability(Long trainerId, LocalDateTime trainingDate) {
         boolean isBusy = trainingRepository
